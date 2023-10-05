f8df81c52ebf8f216fe9191f7838d7019db22596
Joelle Johnson <joellejohnson@Joelles-MacBook-Air.local>
2023-10-05 15:47:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const nums = [3, 5, 7, 2]; //Initializing an array of numbers
+const nums2 = [3, 4, 6, 4];
+
+// console.log(nums[2]); 
+
+let fruits = ['banana', 'apple', 'kiwi']; //This is an array of strings
+
+// console.log(fruits[1]);
+
+fruits[0] = 'pineapple';
+
+// console.log(fruits[0]);
+
+let things = [6, 'Todd', false, "false", [2, 5]];
+
+console.log(things[4]);
